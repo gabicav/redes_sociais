47d196ac7bd5fbdb5c39ec69ca383c994d38db62
gabicav <148063809+gabicav@users.noreply.github.com>
2024-10-16 10:12:24 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,16 @@
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+
+async function visualizarInformacoesGblobais()
+ const res = await ferch(url)
+const dados = await res.json()
+console.log(dados);
+}
+
+visualizarInformacoesGlobais()
+
+
+
+
+
+
 
